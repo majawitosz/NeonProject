045e6b109140b500e8dd5234496c4654813279ff
Maja <majawitosz@gmail.com>
2024-11-19 18:07:03 +0100

    popraiwony asm

--- Asm/Asm.asm
+++ Asm/Asm.asm
@@ -1,28 +1,23 @@
 .code
 MyProc1 proc
-
-; RCX = current pixel RGB values packed
-    ; RDX = neighbor pixel RGB values packed
+    mov r8d, ecx        ; RCX = pierwszy piksel
+    mov r9d, edx        ; RDX = drugi piksel
     
-    ; Extract R, G, B components
-    mov r8d, ecx        ; Current pixel
-    mov r9d, edx        ; Neighbor pixel
-    
-    ; Calculate absolute differences for R
+    ;Porownanie czesci R
     mov eax, r8d
-    and eax, 0FF0000h   ; Mask for R
+    and eax, 0FF0000h   ; Maska dla R
     shr eax, 16
     mov r10d, r9d
     and r10d, 0FF0000h
     shr r10d, 16
     sub eax, r10d
-    jns @f              ; Skip if positive
+    jns @f             
     neg eax
-@@: mov r11d, eax       ; Store R difference
+@@: mov r11d, eax      
     
-    ; Calculate absolute differences for G
+    ;czesc G
     mov eax, r8d
-    and eax, 0FF00h     ; Mask for G
+    and eax, 0FF00h     ; Maska dla G
     shr eax, 8
     mov r10d, r9d
     and r10d, 0FF00h
@@ -30,26 +25,26 @@ MyProc1 proc
     sub eax, r10d
     jns @f
     neg eax
-@@: add r11d, eax       ; Add G difference
+@@: add r11d, eax       
     
-    ; Calculate absolute differences for B
+    ;czesc B
     mov eax, r8d
-    and eax, 0FFh       ; Mask for B
+    and eax, 0FFh       ;Maska dla B
     mov r10d, r9d
     and r10d, 0FFh
     sub eax, r10d
     jns @f
     neg eax
-@@: add r11d, eax       ; Add B difference
+@@: add r11d, eax       
     
-    ; Compare with threshold and set edge value
+    ;ustawienie krawedzi
     mov eax, r11d
-    cmp eax, 50         ; Adjustable threshold
-    jl no_edge
-    mov eax, 255        ; Edge detected
+    cmp eax, 50         ;porownanie z progiem
+    jl no_edge          ;Je?li mniejsze - brak kraw?dzi
+    mov eax, 255        ;Je?li wi?ksze - wykryto kraw?d?
     jmp done
 no_edge:
-    xor eax, eax        ; No edge
+    xor eax, eax        
 done:
     ret
 MyProc1 endp
